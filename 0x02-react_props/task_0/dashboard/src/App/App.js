3be94646993010ef78d11b9884c57ae62afe1d24
george <georgev421@gmail.com>
2023-04-24 16:03:48 +0100

    task 0

--- 0x02-react_props/task_0/dashboard/src/App/App.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.js
@@ -8,7 +8,7 @@ import './App.css';
 const App = () => {
   return (
     <React.Fragment>
-      <Notification />
+      <Notifications />
       <div className="App">
         <Header />
         <Login />
