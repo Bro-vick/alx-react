e8f10ec1eb918c6029011da524749bd8ed9fa3bd
george <georgev421@gmail.com>
2023-04-24 15:52:13 +0100

    task 0

---
+++ 0x02-react_props/task_0/dashboard/src/App/App.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
+
+describe("App Component", () => {
+  it("renders without crashing", () => {
+    const component = shallow(<App />);
+  
+    expect(component).toBeDefined();
+  });
+});
---
+++ 0x02-react_props/task_0/dashboard/src/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+        <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>School dashdoard</h1>
+        </div>
+    </>
+  );
+}
+
+export default Header
